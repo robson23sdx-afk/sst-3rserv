cb7b8fdea53d1c66964b2cc40e5c36e2607d5746
robson23sdx-afk <robson23sdx@gmail.com>
2026-01-17 08:44:16 -0300

    Create Home.jsx
    Cria página Home

---
+++ src/src/components/src/components/src/components/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import Checklist from "../components/Checklist";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20, fontFamily: "Arial" }}>
+      <Header />
+      <Checklist />
+    </div>
+  );
+}
