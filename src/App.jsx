42f927546a7683e01f8ac0ebd140c673a173b194
robson23sdx-afk <robson23sdx@gmail.com>
2026-01-17 08:48:52 -0300

    Update App.jsx
    Atualiza App para nova estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-import { useState } from "react";
+import Home from "./pages/Home";
 
 export default function App() {
-  const [obra, setObra] = useState("");
-
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Inspeção SST – 3R SERV</h1>
-
-      <input
-        placeholder="Nome da obra"
-        value={obra}
-        onChange={(e) => setObra(e.target.value)}
-      />
-
-      <br /><br />
-
-      <button onClick={() => alert("Relatório gerado!")}>
-        Gerar relatório
-      </button>
-    </div>
-  );
+  return <Home />;
 }
