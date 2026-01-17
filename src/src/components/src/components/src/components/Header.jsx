abb58ab4fec1843ec54e86bf4a883bbc537b1cdc
robson23sdx-afk <robson23sdx@gmail.com>
2026-01-17 08:41:44 -0300

    Create Header.jsx
    Cria Header

---
+++ src/src/components/src/components/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header style={{ marginBottom: 20 }}>
+      <h1>Inspeção SST – 3R SERV</h1>
+      <p>Construção • Obras • Saneamento</p>
+      <hr />
+    </header>
+  );
+}
